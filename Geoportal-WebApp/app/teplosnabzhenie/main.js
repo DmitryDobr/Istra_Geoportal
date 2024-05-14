a26d0d5c4b7b12e895119c516ab0d7a36b966c0f
Neuro Linker <63702962+DmitryDobr@users.noreply.github.com>
2024-05-14 17:50:54 +0300

    Script, page, ui update
    - test for measure tool in teplosnabzhenie page

--- Geoportal-WebApp/app/teplosnabzhenie/main.js
+++ Geoportal-WebApp/app/teplosnabzhenie/main.js
@@ -1,7 +1,8 @@
 import '../_src/styles/style.css';
 import '../_src/styles/mapstyle.css';
+import '../_src/styles/tablestyle.css';
 
-import {View} from 'ol';
+import {Feature, View} from 'ol';
 import {Map as OLMap} from 'ol';
 import {ScaleLine, defaults as defaultControls} from 'ol/control.js';
 import Overlay from 'ol/Overlay.js';
@@ -11,13 +12,26 @@ import loadTileLayers from '../_src/scripts/loadTile';
 import initLayerControlGroup from '../_src/scripts/addControlGroup';
 import loadVectorLayers from '../_src/scripts/loadVectorLayer';
 
-
 import JSONDataHeat from './layers_heat.json'
+
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { LineString, Point } from 'ol/geom';
+
+import {getLength} from 'ol/sphere';
 
 var map = null
 const container = document.getElementById('popup');
 const content = document.getElementById('popup-content');
 const closer = document.getElementById('popup-closer');
+
+const table = document.getElementById('measureTable');
+const resultElem = document.getElementById('measureRes');
+
+let mode = 2
+// 1 - получение инфы об объектах на карте
+// 2 - линейное измерение
+// 3 - полигональное измерение
 
 const overlay = new Overlay({
   element: container,
@@ -62,6 +76,64 @@ function showInfo(event) {
   }
 }
 
+var prevPoint = null
+var PointVectorSource = new VectorSource()
+var pointMeasureLayer = new VectorLayer({
+  source: PointVectorSource
+})
+var linevectorSource = new VectorSource()
+var lineMeasureLayer = new VectorLayer({
+  source: linevectorSource
+})
+var MeasureRes = 0
+
+function measuretool(event) {
+
+  let len = ""
+
+  var thing = new Point(event.coordinate)
+
+  if (prevPoint !== null)
+  {
+    var LineThing = new LineString([])
+    LineThing.appendCoordinate(prevPoint)
+    LineThing.appendCoordinate(event.coordinate)
+    var linefeature = new Feature({
+      name: "Thing",
+      geometry: LineThing
+    })
+
+    linevectorSource.addFeature( linefeature );
+    
+    len = getLength(LineThing)
+    MeasureRes += len
+    len = len.toFixed(3)
+  }
+
+  var feature = new Feature({
+    name: "Thing",
+    geometry: thing
+  })
+
+  PointVectorSource.addFeature( feature );
+
+  prevPoint = event.coordinate
+
+  table.innerHTML += "<div>" + event.coordinate[0].toFixed(3) + "</div>" + 
+  "<div>"+ event.coordinate[1].toFixed(3) +"</div>" + 
+  "<div>"+ len +"</div>";
+
+  resultElem.innerHTML = MeasureRes.toFixed(3) + " м";
+}
+function measureClear() {
+  table.innerHTML = ""
+  resultElem.innerHTML = ""
+  PointVectorSource.clear()
+  linevectorSource.clear()
+  MeasureRes = 0
+  prevPoint = null
+}
+
 function initMap() {
   map = new OLMap({
     controls: defaultControls().extend([
@@ -79,7 +151,7 @@ function initMap() {
     overlays: [overlay]
   });
 
-  map.on('singleclick', showInfo);
+  map.on('singleclick', (event) =>{mode === 1 ? showInfo(event) : measuretool(event)});
   map.on('movestart', closeOverlay);
 
   var layers1 = loadTileLayers()
@@ -89,12 +161,15 @@ function initMap() {
   var layers = loadVectorLayers(JSONDataHeat); // zagruzka sloev karti teplosnabzenia
   for (var l of layers) {map.addLayer(l[1])}
   initLayerControlGroup("Схема теплоснабжения г.Истра", layers)
+
+  map.addLayer(lineMeasureLayer)
+  map.addLayer(pointMeasureLayer)
 }
 
 initMap();
 
 
-
+// скрыть показать переключаемую "легенду"
 function LayerControlBoxVChange() {
   var x = document.getElementById("LayerControlBox");
   // console.log(x)
@@ -109,6 +184,7 @@ function LayerControlBoxVChange() {
 }
 document.getElementById("ControlButton").addEventListener("click", LayerControlBoxVChange);
 
+// скрыть показать навигацию по сайту
 function MapNavigationVChange() {
   var x = document.getElementById("MapNavigation");
   // console.log(x)
@@ -122,3 +198,20 @@ function MapNavigationVChange() {
   x.style.display = "none";
 }
 document.getElementById("menuButton").addEventListener("click", MapNavigationVChange);
+
+// скрыть показать инструменты измерений
+function MeasureControlBoxVChange() {
+  var x = document.getElementById("measureButtonsBox");
+  // console.log(x)
+  if (x.style.display === "none") {
+      x.style.display = "";
+      mode = 2
+  } else {
+      x.style.display = "none";
+      mode = 1
+  }
+  console.log(mode)
+}
+document.getElementById("measureControlButton").addEventListener("click", MeasureControlBoxVChange);
+
+document.getElementById("MeasureClear").addEventListener("click", measureClear);
